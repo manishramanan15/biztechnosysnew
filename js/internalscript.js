f351a0877b1b44aac757528c1a68edbb1c77cfd0
maram <manishramanan15@gmail.com>
2020-01-08 14:03:29 +0530

    Release 8th Jan 2020
    Signed-off-by: maram <manishramanan15@gmail.com>

---
+++ js/internalscript.js
@@ -0,0 +1,9 @@
+﻿
+function goToByScroll(id) {
+    console.log(id);
+    $('html,body').animate({ scrollTop: $("#" + id).offset().top - 130 }, 'slow');
+}
+var url = window.location.href;
+var id = url.substring(url.lastIndexOf('#') + 1);
+
+goToByScroll(id);
